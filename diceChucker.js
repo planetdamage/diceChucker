bd614b14d893e735513f0a5b86df6b9a1825eb05
planetdamage <45301523+planetdamage@users.noreply.github.com>
2022-10-15 23:16:38 +0200

    Update diceChucker.js

--- diceChucker.js
+++ diceChucker.js
@@ -1,18 +1,20 @@
-/*This exercise is about figuring out efficient ways to work with physical polyhedral dice (typically used in RPGs, see d4, d6, d8, d10, d12 and d20) when rolling for results on tables.
+/*This exercise is about figuring out efficient ways to work with  polyhedral dice (typically used in RPGs, see d4, d6, d8, d10, d12 and d20) when rolling for results on tables.
 
-NOTE: I left out non-standard (and hard-to-acquire) RPG dice such as d30, d100 (the one which is one die and not a combination of two d10's) or even d14, d16, etc. Only standard RPG dice set dice are used here.
+NOTE: Only standard RPG dice (see above) are used here. I ignored hard-to-acquire non-standard RPG dice such as d30.
+
 
 QUESTION: If you have a table with n (user input) elements to roll for, what is the ideal combination of dice you have to roll so that you can roll for all possible options on the table exactly once?
+OR IN OTHER WORDS: If you have a table of 10 armor types or 20 weapon types to roll for, that's easy to handle. But what about tables where you have irregular numbers, such as 14 or 17 or 19?
 
-PREFERENCES: 1) use d4 as little as possible 2) use high coefficient results as much as possible 3) least amount of dice
+PREFERENCES: 1) use d4 as little as possible 2) use high coefficient results as much as possible 3) least amount of dice wins
 
 
 STUFF TO COMPLETE:
 -Integer check
 -Combine messages into one line
--Step 4, combination of different dice with multiple coefficients
--Step 4b, variation of Step4 where coeff1 becomes --;
+-Filter results based on preferences (try to ignore d4, high coefficient wins, least amount of dice wins)
 */
+
 
 //Get user input
 var tot = prompt("How many elements does your list have?");
@@ -30,7 +32,7 @@ Checking user input integer based on increasingly difficult cases ---
    INCLUDES MERGED CASE of "Solvable with exactly one die"
 3) Solvable with product arithmeticals of dice (d44, d666, d486, etc.
    INCLUDES MERGED CASE of "Solvable with product arithmeticals of exactly one kind of die"
-4) Solvable with a combination of dice with different coefficients
+4) Solvable with a combination of dice with different coefficients.
 
 
 
@@ -54,12 +56,21 @@ for (j = 0; j < diceType.length; j++) {
   }
 }
 
-//CASE 3 - Solvable with prod.arithmeticals of three different dice
+//CASE 3a - Solvable with prod.arithmeticals of three different dice
+for (fD = 0; fD < diceType.length; fD++) {
+  for (sD = 0; sD < diceType.length; sD++) {
+      if (tot == diceType[fD] * diceType[sD]) {
+        console.log("3b) Roll a d" + diceType[fD] + diceType[sD]);
+    }
+  }
+}
+
+//CASE 3b - Solvable with prod.arithmeticals of three different dice
 for (fD = 0; fD < diceType.length; fD++) {
   for (sD = 0; sD < diceType.length; sD++) {
     for (tD = 0; tD < diceType.length; tD++) {
       if (tot == diceType[fD] * diceType[sD] * diceType[tD]) {
-        console.log("4) Roll a d" + diceType[fD] + diceType[sD] + diceType[tD]);
+        console.log("3b) Roll a d" + diceType[fD] + diceType[sD] + diceType[tD]);
       }
     }
   }
@@ -71,9 +82,8 @@ var dicePrime = [19, 11, 9, 7, 5, 3];
 var diceOneCoeff = [0, 0, 0, 0, 0, 0];
 var diceTwoCoeff = [0, 0, 0, 0, 0, 0];
 var totRemainder = tot;
-var case4text = "";
 
-function gcDice(a, b) {
+function GCD(a, b) {
   var remainder = a % b;
   var gcd = (a - remainder) / b;
   return gcd;
@@ -81,21 +91,15 @@ function gcDice(a, b) {
 
 for (i = 0; i < diceType.length; i++) {
   for (j = 0; j < diceType.length; j++) {
-    diceOneCoeff[i] = gcDice(tot, diceType[i]);
+	
+    diceOneCoeff[i] = GCD(tot, diceType[i]);		
     var chunk1 = diceOneCoeff[i] * diceType[i]; 
     totRemainder = tot - chunk1 + diceOneCoeff[i]-1;
 
-    diceTwoCoeff[j] = gcDice(totRemainder, dicePrime[j]);
-		
-		
-		
-		
-    var chunk2 = diceOneCoeff[j] * dicePrime[j];
-		var bonusz = diceOneCoeff[i]+diceTwoCoeff[j]-1;
+    diceTwoCoeff[j] = GCD(totRemainder, dicePrime[j]);
+    var chunk2 = diceTwoCoeff[j] * dicePrime[j];
 
-
-    var biggie = chunk1 + chunk2 - bonusz;
-    if (tot - biggie == 0 && diceOneCoeff[i] !== 0 && diceTwoCoeff[j] !== 0) {
+    if (tot - (chunk1+chunk2) == 0 && diceOneCoeff[i] !== 0 && diceTwoCoeff[j] !== 0) {
       console.log("4) Roll a " + diceOneCoeff[i] + "d" + diceType[i] + "+" + diceTwoCoeff[j] + "d" + diceType[j]);
     }
 
